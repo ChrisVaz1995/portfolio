37198ba42e3458a2987211a9d64212126c25179b
ChrisVaz1995 <christopher.vazquez2015@gmail.com>
2022-04-26 19:30:55 -0500

    added linkedin and github logo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,8 +8,8 @@ export const Hero = () => {
                 <h1 className='hero-title'>Hero World! My Name is Chris Vazquez</h1>
                 <h3 className='hero-title'>I am a Front-End Engineer / Software Engineer</h3>
                 <div className="wrapper">
-                <div>Github</div>
-                <div>Linkedin</div>
+                <div><a href="https://github.com/ChrisVaz1995" target="_blank"><img src={require('../img/github.jpg')}></img> </a></div>
+                <div><a href="https://www.linkedin.com/in/chrisvazquez95/" target="_blank"><img src={require('../img/linkedin.png')}></img></a></div>
                 </div>
             </div>
         </div>
